45bad41bb41879fe0a4fe8142e109f7088ad1618
Nick Rose <nicholas.rose@cbc.ca>
2019-01-31 19:44:59 -0500

    Abort Search entity insert if there's a conflict - I don't want it to override existing rows that might have isSubscribed set

--- data/src/main/java/nick/data/dao/SearchesDao.kt
+++ data/src/main/java/nick/data/dao/SearchesDao.kt
@@ -2,11 +2,16 @@ package nick.data.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import nick.data.model.Search
 
 @Dao
 interface SearchesDao : BaseDao<Search> {
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    override fun insert(entity: Search)
 
     @Query("SELECT * FROM searches")
     fun queryAll(): LiveData<List<Search>>
